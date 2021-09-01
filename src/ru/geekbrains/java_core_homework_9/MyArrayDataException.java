3dd85c486b86c363f2310f5cadd6ade1ca466a9e
lekoal <coreglus@gmail.com>
2021-09-01 14:41:42 +0300

    Initial commit

---
+++ src/ru/geekbrains/java_core_homework_9/MyArrayDataException.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.java_core_homework_9;
+
+public class MyArrayDataException extends Exception{
+@Override
+    public String toString() {
+        return "Cant convert symbol to int!";
+    }
+}
---
+++ src/ru/geekbrains/java_core_homework_9/MyArraySizeException.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.java_core_homework_9;
+
+public class MyArraySizeException extends Exception{
+@Override
+    public String toString() {
+        return "Array size is wrong!";
+    }
+}
